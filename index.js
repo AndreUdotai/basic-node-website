78280df96502235e7635c108b8db1b6d0ffa726e
Ubong Udotai <31491736+AndreUdotai@users.noreply.github.com>
2022-10-04 15:08:54 +0100

    wrote the server codes with express

--- index.js
+++ index.js
@@ -1,58 +1,89 @@
-const http = require('http');
+// const http = require('http');
+// const path = require('path');
+// const fs = require('fs');
+
+// const port = process.env.PORT || 8080;
+
+// const server = http.createServer((req, res) => {
+//     // Build file path
+//     let filePath = path.join(
+//         __dirname,
+//         req.url === '/' ? 'index.html' : req.url + '.html',
+//     );
+
+//     // Extension of the file
+//     let extname = path.extname(filePath);
+
+//     // Initial content type
+//     let contentType = 'text/html';
+
+//     // CHeck ext and set content type
+//     switch(extname){
+//       case '.js':
+//         contentType = 'text/javascript';
+//         break;
+//       case '.css':
+//         contentType = 'text/css';
+//         break;
+//       case '.json':
+//         contentType = 'application.json';
+//         break;
+//     }
+
+//     // Read file
+//     fs.readFile(filePath, (err, content) => {
+//       if(err){
+//         if(err.code == 'ENOENT'){
+//           // page not found
+//           fs.readFile(path.join(__dirname, '404.html'), 
+//           (err, content) => {
+//             res.writeHead(200, {'content-Type': contentType })
+//             res.end(content, 'utf-8')
+//           })
+//         } else {
+//           // SOme server error
+//           res.writeHead(500);
+//           res.end(`Server Error: ${err.code}`);
+//         }
+//       } else {
+//         // Success
+//         res.writeHead(200, {'content-Type':'text/html' })
+//         res.end(content, 'utf-8')
+//       }
+//     })
+// });
+
+// server.listen(port, () => {
+//     console.log(`Server running at port ${port}`);
+// });
+
+const express = require('express');
+const app = express();
 const path = require('path');
-const fs = require('fs');
+const router = express.Router();
 
-const port = process.env.PORT || 8080;
+const port = 3000;
 
-const server = http.createServer((req, res) => {
-    // Build file path
-    let filePath = path.join(
-        __dirname,
-        req.url === '/' ? 'index.html' : req.url + '.html',
-    );
-
-    // Extension of the file
-    let extname = path.extname(filePath);
-
-    // Initial content type
-    let contentType = 'text/html';
-
-    // CHeck ext and set content type
-    switch(extname){
-      case '.js':
-        contentType = 'text/javascript';
-        break;
-      case '.css':
-        contentType = 'text/css';
-        break;
-      case '.json':
-        contentType = 'application.json';
-        break;
-    }
-
-    // Read file
-    fs.readFile(filePath, (err, content) => {
-      if(err){
-        if(err.code == 'ENOENT'){
-          // page not found
-          fs.readFile(path.join(__dirname, '404.html'), 
-          (err, content) => {
-            res.writeHead(200, {'content-Type': contentType })
-            res.end(content, 'utf-8')
-          })
-        } else {
-          // SOme server error
-          res.writeHead(500);
-          res.end(`Server Error: ${err.code}`);
-        }
-      } else {
-        // Success
-        res.writeHead(200, {'content-Type':'text/html' })
-        res.end(content, 'utf-8')
-      }
-    })
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+    //__dirname : It will resolve to your project folder.
 });
 
-server.listen(port, () => {
-    console.log(`Server running at port ${port}`);
+router.get('/about', function (req, res) {
+    res.sendFile(path.join(__dirname + '/about.html'));
+});
+
+router.get('/contact-me', function (req, res) {
+    res.sendFile(path.join(__dirname + '/contact-me.html'));
+});
+
+router.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/404.html'));
+});
+
+//add the router
+app.use('/', router);
+
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`);
 });
